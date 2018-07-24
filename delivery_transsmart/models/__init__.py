f2c8074335d8b0807dc04eb45234b605d5129922
George Daramouskas <gdaramouskas@therp.nl>
2018-07-24 15:02:52 +0200

    [DEL] #4801 Remove carrier and rate setting on the stock.picking from the delivery order.

--- delivery_transsmart/models/__init__.py
+++ delivery_transsmart/models/__init__.py
@@ -3,7 +3,6 @@
 # © 2017 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import http_request_log
-from . import sale_order
 from . import product_product
 from . import delivery_service_level
 from . import stock_picking
